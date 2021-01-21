7a5fd9eaaa5d61284d864a9abee8239b8bd89008
Alon Amsalem <alona@codevalue.net>
2021-01-21 19:29:02 +0200

    copy paste widgets context menu

---
+++ src/Apps/Windows/AnyStatus.Apps.Windows/Features/ContextMenu/Items/200_CopyPaste.cs
@@ -0,0 +1,38 @@
+﻿using AnyStatus.API.Widgets;
+using AnyStatus.Apps.Windows.Infrastructure.Mvvm;
+using AnyStatus.Core.ContextMenu;
+
+namespace AnyStatus.Apps.Windows.Features.ContextMenu.Items
+{
+    public class CopyPaste
+    {
+        private static IWidget _clone;
+
+        public class Copy<T> : ContextMenu<T> where T : class, IWidget, ICopyable
+        {
+            public Copy()
+            {
+                Order = 200;
+                Name = "Copy";
+                Command = new Command(_ => _clone = Context.Clone() as IWidget);
+            }
+        }
+
+        public class Paste<T> : ContextMenu<T> where T : class, IWidget, IAddWidget
+        {
+            public Paste()
+            {
+                Order = 201;
+                Name = "Paste";
+                Command = new Command(_ =>
+                {
+                    Context.Add(_clone);
+                    Context.Expand();
+                    _clone = null;
+                });
+            }
+
+            public override bool IsVisible => _clone is IWidget;
+        }
+    }
+}
--- src/Core/AnyStatus.API/Widgets/IStandardWidget.cs
+++ src/Core/AnyStatus.API/Widgets/IStandardWidget.cs
@@ -1,6 +1,6 @@
 ﻿namespace AnyStatus.API.Widgets
 {
-    public interface IStandardWidget : IRefreshable, IConfigurable, IEnablable, IDeletable, IDuplicatable, IMovable
+    public interface IStandardWidget : IRefreshable, IConfigurable, IEnablable, IDeletable, IDuplicatable, IMovable, ICopyable
     {
     }
 }
