b1f52c27d516eaec03e5172b960186f03b9dd8c4
Alon Amsalem <alona@codevalue.net>
2021-04-19 19:51:03 +0300

    demo notifications widget

---
+++ src/Plugins/AnyStatus.Plugins/Demo/DemoNotificationsWidget.cs
@@ -0,0 +1,30 @@
+﻿using AnyStatus.API.Notifications;
+using AnyStatus.API.Widgets;
+using System.ComponentModel;
+
+namespace AnyStatus.Plugins.Demo
+{
+    [Category("AnyStatus")]
+    [DisplayName("Notifications Demo")]
+    [Description("An example for sending desktop notifications from widgets.")]
+    public class DemoNotificationsWidget : TextLabelWidget, IPollable, IStandardWidget
+    {
+    }
+
+    public class DemoNotificationsHandler : StatusCheck<DemoNotificationsWidget>
+    {
+        private readonly INotificationService _notificationService;
+
+        public DemoNotificationsHandler(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        protected override void Handle(StatusRequest<DemoNotificationsWidget> request)
+        {
+            _notificationService.Send(new Notification("Message", "Title", NotificationIcon.Info));
+
+            request.Context.Status = Status.OK;
+        }
+    }
+}
