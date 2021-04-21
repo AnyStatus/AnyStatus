f4c937840d61c59cbfd9e071232ea3bffd007809
Alon Amsalem <alona@codevalue.net>
2021-04-21 20:31:30 +0300

    property builder refactoring

---
+++ src/Apps/Windows/AnyStatus.Apps.Windows/Infrastructure/Controls/PropertyGrid/IPropertyBuilder.cs
@@ -0,0 +1,10 @@
+﻿using AnyStatus.Apps.Windows.Infrastructure.Mvvm.Controls.PropertyGrid;
+using System.Collections.Generic;
+
+namespace AnyStatus.Apps.Windows.Infrastructure.Controls.PropertyGrid
+{
+    internal interface IPropertyBuilder
+    {
+        IEnumerable<IProperty> Build(object source);
+    }
+}
--- src/Apps/Windows/AnyStatus.Apps.Windows/Infrastructure/Controls/PropertyGrid/PropertyGridViewModel.cs
+++ src/Apps/Windows/AnyStatus.Apps.Windows/Infrastructure/Controls/PropertyGrid/PropertyGridViewModel.cs
@@ -1,32 +1,20 @@
-﻿using AnyStatus.API.Attributes;
-using AnyStatus.Core.Extensions;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using AnyStatus.Apps.Windows.Infrastructure.Controls.PropertyGrid;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace AnyStatus.Apps.Windows.Infrastructure.Mvvm.Controls.PropertyGrid
 {
     internal class PropertyGridViewModel : BaseViewModel, IPropertyGridViewModel
     {
         private object _target;
-        private readonly ILogger _logger;
         private IEnumerable<IProperty> _properties;
-        private readonly IServiceProvider _serviceProvider;
 
-        public PropertyGridViewModel(IServiceProvider serviceProvider, ILogger logger)
+        public PropertyGridViewModel(IPropertyBuilder propertyBuilder)
         {
-            _logger = logger;
-            _serviceProvider = serviceProvider;
-
-            PropertyChanged += (s, e) =>
+            PropertyChanged += (_, e) =>
             {
-                if (e.PropertyName.Equals(nameof(Target)))
+                if (e.PropertyName.Equals(nameof(Target)) && Target is object)
                 {
-                    Properties = Target is object ? BuildProperties(Target).ToList() : null;
+                    Properties = propertyBuilder.Build(Target);
                 }
             };
         }
@@ -41,161 +29,6 @@ namespace AnyStatus.Apps.Windows.Infrastructure.Mvvm.Controls.PropertyGrid
         {
             get => _properties;
             set => Set(ref _properties, value);
-        }
-
-        private IEnumerable<IProperty> BuildProperties(object source)
-        {
-            if (source is null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-
-            var properties = source.GetType().GetProperties().Where(p => p.CanWrite && p.IsBrowsable()).OrderBy(p => p.Order()).ToList();
-
-            foreach (var propertyInfo in properties)
-            {
-                yield return BuildProperty(source, propertyInfo);
-            }
-        }
-
-        private IProperty BuildProperty(object source, PropertyInfo propertyInfo)
-        {
-            IProperty property = null;
-
-            if (Attribute.IsDefined(propertyInfo, typeof(ItemsSourceAttribute)))
-            {
-                var attribute = propertyInfo.GetCustomAttribute<ItemsSourceAttribute>();
-
-                if (_serviceProvider.GetService(attribute.Type) is IItemsSource itemsSource)
-                {
-                    var dropDown = new DropDownProperty(propertyInfo, source)
-                    {
-                        Name = propertyInfo.Name
-                    };
-
-                    property = dropDown;
-
-                    dropDown.Refresh = () =>
-                    {
-                        dropDown.Items = null;
-
-                        Task.Run(() =>
-                        {
-                            try
-                            {
-                                dropDown.Items = itemsSource.GetItems(source);
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger.LogError(ex, "An error occurred while creating items source '{type}' for property '{property}'.", attribute.Type.Name, propertyInfo.Name);
-                            }
-                        });
-                    };
-
-                    if (propertyInfo.GetCustomAttribute<RefreshAttribute>() is RefreshAttribute refreshAttribute)
-                    {
-                        dropDown.SelectionChanged = new Command(_ =>
-                        {
-                            foreach (var property in Properties)
-                            {
-                                if (property is DropDownProperty dropDownProperty && dropDownProperty.Name.Equals(refreshAttribute.Name))
-                                {
-                                    dropDownProperty.Refresh();
-                                }
-                            }
-                        });
-                    }
-
-                    if (attribute.Autoload)
-                    {
-                        dropDown.Refresh();
-                    }
-                }
-            }
-            else if (Attribute.IsDefined(propertyInfo, typeof(AsyncItemsSourceAttribute)))
-            {
-                var attribute = propertyInfo.GetCustomAttribute<AsyncItemsSourceAttribute>();
-
-                if (_serviceProvider.GetService(attribute.Type) is IAsyncItemsSource asyncItemsSource)
-                {
-                    var dropDown = new DropDownProperty(propertyInfo, source)
-                    {
-                        Name = propertyInfo.Name
-                    };
-
-                    property = dropDown;
-
-                    dropDown.Refresh = () =>
-                    {
-                        dropDown.Items = null;
-
-                        Task.Run(async () =>
-                        {
-                            try
-                            {
-                                dropDown.Items = await asyncItemsSource.GetItemsAsync(source);
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger.LogError(ex.GetBaseException(), "An error occurred while executing items-source '{type}' for property '{property}'.", attribute.Type.Name, propertyInfo.Name);
-                            }
-                        });
-                    };
-
-                    if (propertyInfo.GetCustomAttribute<RefreshAttribute>() is RefreshAttribute refreshAttribute)
-                    {
-                        dropDown.SelectionChanged = new Command(_ =>
-                        {
-                            foreach (var property in Properties)
-                            {
-                                if (property is DropDownProperty dropDownProperty && dropDownProperty.Name.Equals(refreshAttribute.Name))
-                                {
-                                    dropDownProperty.Refresh();
-                                }
-                            }
-                        });
-                    }
-
-                    if (attribute.Autoload)
-                    {
-                        dropDown.Refresh();
-                    }
-                }
-            }
-            else if (propertyInfo.PropertyType.IsEnum)
-            {
-                property = new DropDownProperty(propertyInfo, source)
-                {
-                    Items = Enum.GetNames(propertyInfo.PropertyType).Select(i => new NameValueItem(i, i))
-                };
-            }
-            else if (propertyInfo.PropertyType == typeof(string))
-            {
-                property = new TextProperty(propertyInfo, source);
-            }
-            else if (propertyInfo.PropertyType == typeof(bool))
-            {
-                property = new BooleanProperty(propertyInfo, source);
-            }
-            else if (propertyInfo.PropertyType.IsNumeric())
-            {
-                property = new NumericProperty(propertyInfo, source);
-            }
-
-            if (property is object)
-            {
-                property.Value = propertyInfo.GetValue(source);
-
-                var displayNameAttribute = propertyInfo.GetCustomAttributes<DisplayNameAttribute>().FirstOrDefault();
-
-                property.Header = displayNameAttribute is null || string.IsNullOrWhiteSpace(displayNameAttribute.DisplayName) ? propertyInfo.Name : displayNameAttribute.DisplayName;
-
-                var readOnlyAttribute = propertyInfo.GetCustomAttributes<ReadOnlyAttribute>().FirstOrDefault();
-
-                property.IsReadOnly = readOnlyAttribute?.IsReadOnly ?? default;
-            }
-
-            return property;
         }
     }
 }
