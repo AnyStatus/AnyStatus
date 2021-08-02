bb149aaff5b12d4f05450576042e75dd2bff88c8
Alon Amsalem <alona@codevalue.net>
2021-08-02 22:48:59 +0300

    minor optimization

--- src/Core/AnyStatus.Core/Pipeline/Behaviors/ValidationBehavior.cs
+++ src/Core/AnyStatus.Core/Pipeline/Behaviors/ValidationBehavior.cs
@@ -1,6 +1,5 @@
 ﻿using AnyStatus.API.Common;
 using MediatR;
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -13,25 +12,20 @@ namespace AnyStatus.Core.Pipeline.Behaviors
     {
         private readonly IValidator<TRequest>[] _validators;
 
-        public ValidationBehavior(IValidator<TRequest>[] validators)
-        {
-            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
-        }
+        public ValidationBehavior(IValidator<TRequest>[] validators) => _validators = validators;
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             var failures = _validators
-                .Select(v => v.Validate(request))
-                .Where(v => v != null)
-                .SelectMany(v => v)
-                .Where(v => v != null)
-                .ToList();
+                 .Select(v => v.Validate(request))
+                 .Where(v => v != null)
+                 .SelectMany(v => v)
+                 .Where(v => v != null)
+                 .ToList();
 
             if (failures.Any())
             {
                 var sb = new StringBuilder();
-
-                sb.AppendLine("Validation Error:");
 
                 foreach (var failure in failures)
                 {
@@ -40,10 +34,8 @@ namespace AnyStatus.Core.Pipeline.Behaviors
 
                 throw new ValidationException(sb.ToString());
             }
-            else
-            {
-                return await next().ConfigureAwait(false);
-            }
+
+            return next();
         }
     }
 }
