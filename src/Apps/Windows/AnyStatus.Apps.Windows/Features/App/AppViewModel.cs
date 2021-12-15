8476688996fab7dc2c97fe1a1af515c1cc6e541b
Alon Amsalem <alona@codevalue.net>
2021-12-15 12:14:15 +0200

    - fixed global refresh key bindings - app view model cleanup

--- src/Apps/Windows/AnyStatus.Apps.Windows/Features/App/AppViewModel.cs
+++ src/Apps/Windows/AnyStatus.Apps.Windows/Features/App/AppViewModel.cs
@@ -1,20 +1,21 @@
 ﻿using AnyStatus.Apps.Windows.Features.Dashboard;
 using AnyStatus.Apps.Windows.Features.Menu;
 using AnyStatus.Apps.Windows.Features.ToolBar;
+using AnyStatus.Apps.Windows.Infrastructure.Mvvm;
 using AnyStatus.Apps.Windows.Infrastructure.Mvvm.Pages;
+using AnyStatus.Core.App;
+using AnyStatus.Core.Jobs;
+using MediatR;
 using System;
+using System.Threading.Tasks;
 
 namespace AnyStatus.Apps.Windows.Features.App
 {
-    public sealed class AppViewModel : IDisposable
+    public sealed class AppViewModel : BaseViewModel, IDisposable
     {
-        public AppViewModel(DashboardViewModel dashboardViewModel, MenuViewModel menuViewModel, ToolBarViewModel toolBarViewModel, PagesViewModel pagesViewModel)
+        public AppViewModel(IMediator mediator, IAppContext context)
         {
-            MenuViewModel = menuViewModel;
-            PagesViewModel = pagesViewModel;
-            ToolBarViewModel = toolBarViewModel;
-            DashboardViewModel = dashboardViewModel;
-            ToolBarViewModel.MenuViewModel = MenuViewModel;
+            Commands.Add("Refresh", new Command(p => Task.Run(async () => await mediator.Send(new Refresh.Request(context.Session.Widget)))));
         }
 
         public void Dispose()
@@ -23,11 +24,12 @@ namespace AnyStatus.Apps.Windows.Features.App
             PagesViewModel = null;
             ToolBarViewModel = null;
             DashboardViewModel = null;
+            Commands.Clear();
         }
 
-        public MenuViewModel MenuViewModel { get; private set; }
-        public PagesViewModel PagesViewModel { get; private set; }
-        public ToolBarViewModel ToolBarViewModel { get; private set; }
-        public DashboardViewModel DashboardViewModel { get; private set; }
+        public MenuViewModel MenuViewModel { get; set; }
+        public PagesViewModel PagesViewModel { get; set; }
+        public ToolBarViewModel ToolBarViewModel { get; set; }
+        public DashboardViewModel DashboardViewModel { get; set; }
     }
 }
