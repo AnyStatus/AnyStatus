31f064d33a1829f9e673658a167b7725c687c79e
Alon Amsalem <alona@codevalue.net>
2022-01-13 17:36:17 +0200

    total jobs widget

---
+++ src/Core/AnyStatus.Core/Jobs/TotalJobsWidget.cs
@@ -0,0 +1,34 @@
+﻿using AnyStatus.API.Widgets;
+using Quartz;
+using Quartz.Impl.Matchers;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnyStatus.Plugins.Quartz
+{
+    [Category("AnyStatus")]
+    [DisplayName("Total Jobs")]
+    [Description("Display the total number of scheduled jobs")]
+    public class TotalJobsWidget : TextWidget, ICommonWidget, IPollable
+    {
+    }
+
+    public class TotalJobsQuery : AsyncStatusCheck<TotalJobsWidget>
+    {
+        private readonly ISchedulerFactory _schedulerFactory;
+
+        public TotalJobsQuery(ISchedulerFactory schedulerFactory) => _schedulerFactory = schedulerFactory;
+
+        protected override async Task Handle(StatusRequest<TotalJobsWidget> request, CancellationToken cancellationToken)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler(cancellationToken);
+
+            var triggerKeys = await scheduler.GetTriggerKeys(GroupMatcher<TriggerKey>.AnyGroup());
+
+            request.Context.Text = triggerKeys.Count.ToString();
+
+            request.Context.Status = Status.OK;
+        }
+    }
+}
